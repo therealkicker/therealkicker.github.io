32e8523ecec9305ee4b0eab7ee51e800a8bab8f7
Tim Schneider <tim@railslove.com>
2013-08-26 11:55:31 +0200

    fixed lamp for desktop version

--- javascript.js
+++ javascript.js
@@ -1,2 +1,18 @@
 $(function() {
+  $(window).scroll(function(){
+      if( $(window).scrollTop() < 71 ) {
+        $('.feature.lamp').removeClass("fixed");
+        $('.feature.lamp').removeClass("absolute");
+        $('.feature.lamp .explanation').fadeIn();
+        }
+      if( $(window).scrollTop() > 70 && $(window).scrollTop() < 221 ) {
+          $('.feature.lamp').addClass("fixed");
+          $('.feature.lamp').removeClass("absolute");
+          $('.feature.lamp .explanation').fadeOut();
+        }
+      if( $(window).scrollTop() > 222 ) {
+          $('.feature.lamp').removeClass("fixed");
+          $('.feature.lamp').addClass("absolute");
+      }
+  });
 });
